6690f92d845270eef47d6307d7ce3c33f70feb39
Railey Bautista <raileybautista420@gmail.com>
2024-07-12 19:22:20 +0800

    Initial commit with BSCRIPT01.bat

---
+++ BSCRIPT01.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Open five websites
+start https://www.google.com
+start https://www.youtube.com
+start https://www.yahoo.com
+start https://www.github.com
+start https://www.Facebook.com
+
+REM Launch Calculator
+start calc.exe
+
+REM Launch Notepad
+start notepad.exe
+
+REM Initiate a system shutdown after a brief delay (e.g., 60 seconds)
+shutdown -s -t 60
