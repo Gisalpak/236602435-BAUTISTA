661354f9314860ecb10e68f47fa0d0937533237e
Railey Bautista <raileybautista420@gmail.com>
2024-07-20 21:04:18 +0800

    Sample program for functionality

---
+++ DemoFiles/Sample.php
@@ -0,0 +1,22 @@
+<?php
+// Print "Hello, World!"
+echo "Hello, World!";
+
+// Database connection details
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Close connection
+$conn->close();
+?>
